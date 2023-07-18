ecdf0b4330877fe5a0a3676c4277df5978264669
ChenHuanOWO <canyu233@gmail.com>
2023-07-18 09:50:26 +0800

    learn to use User-defined function

---
+++ add.c
@@ -0,0 +1,18 @@
+
+#include <stdio.h>
+
+int Add(int x,int y)
+{
+	int z = x + y;
+	return z;
+}
+
+int main()
+{
+	int sum = 0;
+	int a = 10;
+	int b = 20;
+	sum = Add(a, b);
+		printf("sum = %d\n", sum);
+	return 0;
+}
